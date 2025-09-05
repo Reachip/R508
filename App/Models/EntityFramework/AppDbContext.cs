feabc8b7af77e1e8279388f393518e11f5d78a46
Rached Mejri <r.mejri74100@gmail.com>
2025-09-05 14:59:45 +0200

    Description des contraintes dans le db context

--- App/Models/EntityFramework/AppDbContext.cs
+++ App/Models/EntityFramework/AppDbContext.cs
@@ -21,6 +21,40 @@ public partial class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Produit>(e =>
+        {
+            e.HasKey(p => p.IdProduit);
+            
+            e.HasOne(p => p.MarqueNavigation)
+                .WithMany(m => m.Produits)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_produits_marque");
+            
+            e.HasOne(p => p.TypeProduitNavigation)
+                .WithMany(m => m.Produits)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_produits_type_produit");
+        });
+        modelBuilder.Entity<TypeProduit>(e =>
+        {
+            e.HasKey(p => p.IdTypeProduit);
+            
+            e.HasMany(p => p.Produits)
+                .WithOne(m => m.TypeProduitNavigation)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_produits_type_produit");
+        });
+        
+        modelBuilder.Entity<Marque>(e =>
+        {
+            e.HasKey(p => p.IdMarque);
+            
+            e.HasMany(p => p.Produits)
+                .WithOne(m => m.MarqueNavigation)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_produits_marque");
+        });
+        
         OnModelCreatingPartial(modelBuilder);
     }
 
